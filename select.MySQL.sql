50ec1bb3e384aaf33bab49894f4682abb751afda
Shannon Sagehorn <srsagehorn@gmail.com>
2020-12-10 12:46:25 -0600

    Finished SQL Selects

--- select.MySQL.sql
+++ select.MySQL.sql
@@ -1,4 +1,4 @@
-___________________DATABASE ___________________
+-- ___________________DATABASE ___________________
 See below db for activities
 
 drop database if exists PersonalTrainer;
@@ -11074,87 +11074,98 @@ insert into InvoiceLineItem(InvoiceId, Description, Price, Quantity, ServiceDate
     (1884, 'Group Instruction', 22.50, 2.00, '2017-10-18');
 
     
-___________________ACTIVITIES ___________________
+-- ___________________ACTIVITIES ___________________
 
 
 
 
-1. Select all rows and columns from the Exercise table. (64 rows)
+-- 1. Select all rows and columns from the Exercise table. (64 rows)
 
 SELECT * FROM Exercise;
 
-2. Select all rows and columns from the Client table. (500 rows)
+-- 2. Select all rows and columns from the Client table. (500 rows)
 
 SELECT * FROM Client;
 
-3. Select all columns from Client where the City is Metairie. (29 rows)
+-- 3. Select all columns from Client where the City is Metairie. (29 rows)
 
 SELECT * FROM Client WHERE City = 'Metairie';
 
-4. Is there a Client with the ClientId '818u7faf-7b4b-48a2-bf12-7a26c92de20c'? (0 rows)
+-- 4. Is there a Client with the ClientId '818u7faf-7b4b-48a2-bf12-7a26c92de20c'? (0 rows)
 
 SELECT * FROM Client WHERE ClientId = '818u7faf-7b4b-48a2-bf12-7a26c92de20c';
 
-5. How many rows are in the Goal table? (17 rows)
+-- 5. How many rows are in the Goal table? (17 rows)
 
 SELECT COUNT(*) FROM Goal;
 
-6. Select Name and LevelId from the Workout table. (26 rows)
+-- 6. Select Name and LevelId from the Workout table. (26 rows)
 
 SELECT Name, LevelId FROM Workout;
 
-7. Select Name, LevelId, and Notes from Workout where LevelId is 2. (11 rows)
+-- 7. Select Name, LevelId, and Notes from Workout where LevelId is 2. (11 rows)
 
 SELECT Name, LevelId, Notes from Workout WHERE LevelId = 2;
 
-8. Select FirstName, LastName, and City from Client where City is Metairie, Kenner, or Gretna. (77 rows)
+-- 8. Select FirstName, LastName, and City from Client where City is Metairie, Kenner, or Gretna. (77 rows)
 
 SELECT FirstName, LastName, City FROM Client WHERE City in ('Metairie', 'Kenner', 'Gretna');
 
-9. Select FirstName, LastName, and BirthDate from Client for Clients born in the 1980s. (72 rows)
+-- 9. Select FirstName, LastName, and BirthDate from Client for Clients born in the 1980s. (72 rows)
 
 SELECT FirstName, LastName, BirthDate FROM Client WHERE BirthDate BETWEEN '1980-01-01' AND '1989-12-31';
 
-10. Write the query above in a different way.
+-- 10. Write the query above in a different way.
 
 If you used BETWEEN, you can't use it again.
 If you didn't use BETWEEN, use it!
 
-11. How many rows in the Login table have a .gov EmailAddress? (17 rows)
+-- 11. How many rows in the Login table have a .gov EmailAddress? (17 rows)
 
 SELECT * FROM Login WHERE EmailAddress LIKE ('%.gov%');
 
-12. How many Logins do NOT have a .com EmailAddress? (122 rows)
+-- 12. How many Logins do NOT have a .com EmailAddress? (122 rows)
 
 SELECT * FROM Login WHERE EmailAddress NOT LIKE ('%.com%');
 
-13. Select first and last name of Clients without a BirthDate. (37 rows)
+-- 13. Select first and last name of Clients without a BirthDate. (37 rows)
 
 SELECT FirstName, LastName FROM Client WHERE BirthDate IS NULL;
 
-14. Select the Name of each ExerciseCategory that has a parent (ParentCategoryId value is not null). (12 rows)
+-- 14. Select the Name of each ExerciseCategory that has a parent (ParentCategoryId value is not null). (12 rows)
 
 SELECT Name FROM ExerciseCategory WHERE ParentCategoryId IS NOT NULL;
 
-15. Select Name and Notes of each level 3 Workout that contains the word 'you' in its Notes. (4 rows)
+-- 15. Select Name and Notes of each level 3 Workout that contains the word 'you' in its Notes. (4 rows)
 
 SELECT Name, Notes FROM Workout WHERE LevelId = 3 AND Notes LIKE ('%you%');
 
-16. Select FirstName, LastName, City from Client whose LastName starts with L,M, or N and who live in LaPlace. (5 rows)
+-- 16. Select FirstName, LastName, City from Client whose LastName starts with L,M, or N and who live in LaPlace. (5 rows)
 
-17. Select InvoiceId, Description, Price, Quantity, ServiceDate and the line item total, a calculated value, from InvoiceLineItem, where the line item total is between 15 and 25 dollars. (667 rows)
+-- doesnt work, doesnt filter out just laplace cities
+SELECT FirstName, LastName, City FROM Client WHERE City = 'LaPlace' AND LastName LIKE ('L%') OR LastName LIKE ('M%') OR LastName LIKE ('N%');
 
-Does the database include an email address for the Client, Estrella Bazely?
+-- 17. Select InvoiceId, Description, Price, Quantity, ServiceDate and the line item total, a calculated value, from InvoiceLineItem, where the line item total is between 15 and 25 dollars. (667 rows)
 
-This requires two queries:
+-- Doesnt work
+SELECT InvoiceId, Description, Price, Quantity, ServiceDate, (Price*Quantity) AS LineItemTotal FROM InvoiceLineItem WHERE LineItemTotal >=15 AND LineItemTotal <=25;
 
-Select a Client record for Estrella Bazely. Does it exist?
-If it does, select a Login record that matches its ClientId.
+-- 18. Does the database include an email address for the Client, Estrella Bazely?
 
-What are the Goals of the Workout with the Name 'This Is Parkour'?
+-- This requires two queries:
 
-You need three queries!
+-- Select a Client record for Estrella Bazely. Does it exist?
+SELECT * FROM Client WHERE FirstName = 'Estrella';
+-- If it does, select a Login record that matches its ClientId.
+SELECT EmailAddress FROM Login WHERE ClientId = '87976c42-9226-4bc6-8b32-23a8cd7869a5';
 
-Select the WorkoutId from Workout where Name equals 'This Is Parkour'. (1 row)
-Select GoalId from WorkoutGoal where the WorkoutId matches the WorkoutId from your first query. (3 rows)
-Select the goal name from Goal where the GoalId is one of the GoalIds from your second query. (3 rows)
+-- 19. What are the Goals of the Workout with the Name 'This Is Parkour'?
+
+-- You need three queries!
+
+-- Select the WorkoutId from Workout where Name equals 'This Is Parkour'. (1 row)
+SELECT WorkoutId FROM Workout WHERE Name = 'This is Parkour';
+-- Select GoalId from WorkoutGoal where the WorkoutId matches the WorkoutId from your first query. (3 rows)
+SELECT GoalId FROM WorkoutGoal WHERE WorkoutId = 12;
+-- Select the goal name from Goal where the GoalId is one of the GoalIds from your second query. (3 rows)
+SELECT Name FROM Goal WHERE GoalId IN (3, 8, 15);
