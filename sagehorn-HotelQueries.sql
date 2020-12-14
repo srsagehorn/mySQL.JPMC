7e7fff668e4f9d9f0c4c4cd2f84ec6b630151080
Shannon Sagehorn <srsagehorn@gmail.com>
2020-12-13 23:48:57 -0600

    hotel queries 2-5

--- sagehorn-HotelQueries.sql
+++ sagehorn-HotelQueries.sql
@@ -33,12 +33,12 @@ WHERE g.Name = 'Mack Simmer';
 -- decimal rounding issue
 
 SELECT m.RoomNumber, r.ReservationId, 
-CASE
-	WHEN (r.Adults + r.Children - m.StandardOccupancy) > 0
-	THEN (m.BasePrice + 10.95 * (r.Adults + r.Children - m.StandardOccupancy))
-    WHEN (r.Adults + r.Children - m.StandardOccupancy) <= 0
-    THEN BasePrice
-	ELSE NULL
+    CASE
+        WHEN (r.Adults + r.Children - m.StandardOccupancy) > 0
+            THEN (m.BasePrice + 10.95 * (r.Adults + r.Children - m.StandardOccupancy)* DATEDIFF(r.EndDate, r.StartDate))
+        WHEN (r.Adults + r.Children - m.StandardOccupancy) <= 0
+            THEN (BasePrice* DATEDIFF(EndDate, StartDate))
+        ELSE NULL
     END AS TotalCost
 	FROM Rooms m
 LEFT JOIN Reservations r
@@ -49,9 +49,29 @@ LEFT JOIN Guests g
 
 -- 5. Write a query that returns all the rooms accommodating at least three guests and that are reserved on any date in April 2023.
 
+-- fault here that a reservation spanning April will not be caught
+
+SELECT r.RoomNumber, g.Name, r.StartDate, r.EndDate
+    FROM Reservations r
+INNER JOIN Guests g
+	ON r.GuestId = g.GuestId
+INNER JOIN Rooms m
+	ON m.RoomNumber = r.RoomNumber
+WHERE m.MaxOccupancy >= 3
+	AND r.StartDate BETWEEN  '2023-04-01' AND '2023-05-01'
+	OR r.EndDate BETWEEN  '2023-04-01' AND '2023-05-01';
+
 
 -- 6. Write a query that returns a list of all guest names and the number of reservations per guest, sorted starting with the guest with the most reservations and then by the guest's last name.
 
+-- Needs to have last name sorting
+
+SELECT g.Name, COUNT(r.ReservationId) AS TotalReservations
+	FROM Guests g
+INNER JOIN Reservations r
+	ON g.GuestId = r.GuestId
+GROUP BY g.Name
+ORDER BY TotalReservations DESC;
 
 -- 7. Write a query that displays the name, address, and phone number of a guest based on their phone number. (Choose a phone number from the existing data.)
 -- For each query, include:
@@ -59,3 +79,12 @@ LEFT JOIN Guests g
 -- The request from this assignment as a comment above the query, including the number
 -- The query itself
 -- The results of the query in a comment under the query
+
+-- (314) -974-8036
+
+SELECT Name, Address, City, ZipCode, PhoneNumber FROM Guests WHERE PhoneNumber = '(314) 974-8036';
+
+-- Results :
+-- Shannon Sagehorn    2000 S Michigan Ave     Chicago     60616   (314) -974-8036
+
+-- Can this really be this easy????
