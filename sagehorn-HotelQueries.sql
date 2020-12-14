8be52262b7592c2f67b4ec3622f499e797759888
Shannon Sagehorn <srsagehorn@gmail.com>
2020-12-13 20:12:55 -0600

    2 join problems

--- sagehorn-HotelQueries.sql
+++ sagehorn-HotelQueries.sql
@@ -1,23 +1,26 @@
--- Write a query that returns a list of reservations that end in July 2023, including the name of the guest, the room number(s), and the reservation dates.
+-- 1. Write a query that returns a list of reservations that end in July 2023, including the name of the guest, the room number(s), and the reservation dates.
 
-SELECT StartDate, EndDate,
+SELECT Name, RoomNumber, StartDate, EndDate
+FROM reservation
+WHERE EndDate BETWEEN  '2023-07-01' AND '2023-07-31';
 
--- Write a query that returns a list of all reservations for rooms with a jacuzzi, displaying the guest's name, the room number, and the dates of the reservation.
+-- 2. Write a query that returns a list of all reservations for rooms with a jacuzzi, displaying the guest's name, the room number, and the dates of the reservation.
 
+SELECT 
 
--- Write a query that returns all the rooms reserved for a specific guest, including the guest's name, the room(s) reserved, the starting date of the reservation, and how many people were included in the reservation. (Choose a guest's name from the existing data.)
-
-
--- Write a query that returns a list of rooms, reservation ID, and per-room cost for each reservation. The results should include all rooms, whether or not there is a reservation associated with the room.
-
-
--- Write a query that returns all the rooms accommodating at least three guests and that are reserved on any date in April 2023.
+-- 3. Write a query that returns all the rooms reserved for a specific guest, including the guest's name, the room(s) reserved, the starting date of the reservation, and how many people were included in the reservation. (Choose a guest's name from the existing data.)
 
 
--- Write a query that returns a list of all guest names and the number of reservations per guest, sorted starting with the guest with the most reservations and then by the guest's last name.
+-- 4. Write a query that returns a list of rooms, reservation ID, and per-room cost for each reservation. The results should include all rooms, whether or not there is a reservation associated with the room.
 
 
--- Write a query that displays the name, address, and phone number of a guest based on their phone number. (Choose a phone number from the existing data.)
+-- 5. Write a query that returns all the rooms accommodating at least three guests and that are reserved on any date in April 2023.
+
+
+-- 6. Write a query that returns a list of all guest names and the number of reservations per guest, sorted starting with the guest with the most reservations and then by the guest's last name.
+
+
+-- 7. Write a query that displays the name, address, and phone number of a guest based on their phone number. (Choose a phone number from the existing data.)
 -- For each query, include:
 
 -- The request from this assignment as a comment above the query, including the number
